51c180430908eb4b4a0fa318291ba8f9f36a1a9d
Divyanshu Rai <88521161+raidivyanshu341419@users.noreply.github.com>
2023-02-24 23:21:21 +0530

    First Commit for Navneet Testing Purpose

---
+++ CSharp_With_Navneet/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_With_Navneet
+{
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter your name");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello" + " " + name);
+            Console.ReadKey();
+        }
+    }
+}
